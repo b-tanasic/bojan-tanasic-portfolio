622a0dcca48ab81ca42d4e941ddb2d3206b4e68f
b-tanasic <tanasicb88@gmail.com>
2022-04-09 17:44:14 +0200

    Updates

---
+++ precache-manifest.b1872e741913862eca8448a6787a6f4a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8852d3395aa5523fbae7",
+    "url": "/bojan-tanasic-portfolio/static/css/main.202ae4a8.chunk.css"
+  },
+  {
+    "revision": "8852d3395aa5523fbae7",
+    "url": "/bojan-tanasic-portfolio/static/js/main.8852d339.chunk.js"
+  },
+  {
+    "revision": "563269779e140bffd403",
+    "url": "/bojan-tanasic-portfolio/static/js/1.56326977.chunk.js"
+  },
+  {
+    "revision": "0438250bb4cc726d6514",
+    "url": "/bojan-tanasic-portfolio/static/js/runtime~main.0438250b.js"
+  },
+  {
+    "revision": "d53e80852678be6e39619dccd55f1df7",
+    "url": "/bojan-tanasic-portfolio/static/media/bojan_tanasic_cv.d53e8085.jpg"
+  },
+  {
+    "revision": "6eacf2b7073056f4b8d7b449df6aac18",
+    "url": "/bojan-tanasic-portfolio/static/media/bojan_tanasic_profile.6eacf2b7.png"
+  },
+  {
+    "revision": "33fb58404e3f337a1af9feca29f19806",
+    "url": "/bojan-tanasic-portfolio/static/media/bojan_tanasic_about.33fb5840.jpg"
+  },
+  {
+    "revision": "743b56d0ab45f08bf2660ca7b4d8e93a",
+    "url": "/bojan-tanasic-portfolio/static/media/Blacksword.743b56d0.woff"
+  },
+  {
+    "revision": "7c9950c4a03cabd1ccc12f0791a1fb5a",
+    "url": "/bojan-tanasic-portfolio/static/media/bg-texture-02.7c9950c4.png"
+  },
+  {
+    "revision": "979eda08a4f08944344b647a73e3d0aa",
+    "url": "/bojan-tanasic-portfolio/index.html"
+  }
+];
